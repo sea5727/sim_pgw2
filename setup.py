3e6f5464563ccb5dd2d26f771f148131849ec63b
ysh8361 <ysh8361@localhost.localdomain>
2020-02-05 14:06:36 +0900

    commit

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from cx_Freeze import setup, Executable
 
 
 include_files = [
+    '/home/ysh8361/.pyenv/versions/3.7.5/envs/sim_pgw2/lib/python3.7/site-packages/cx_Freeze',
     '/home/ysh8361/.pyenv/versions/3.7.5/envs/sim_pgw2/lib/python3.7/site-packages/pyfiglet',
     '/home/ysh8361/.pyenv/versions/3.7.5/envs/sim_pgw2/lib/python3.7/site-packages/gi']
-includes = ['pyfiglet', 'gi']
+includes = ['pyfiglet', 'gi', 'cx_Freeze']
 excludes = []
 packages = []
 
@@ -18,6 +19,6 @@ setup(name="test",
           'packages': packages,
           'includes': includes,
           'include_files': include_files,
-          'path': sys.path + ['./build/exe.linux-x86_64-3.7/lib/pyfiglet', './build/exe.linux-x86_64-3.7/lib/gi']
+          'path': sys.path + ['./build/exe.linux-x86_64-3.7/lib/pyfiglet', './build/exe.linux-x86_64-3.7/lib/gi',  './build/exe.linux-x86_64-3.7/lib/cx_Freeze']
       }},
       executables=[Executable("main.py", base='Console')])
